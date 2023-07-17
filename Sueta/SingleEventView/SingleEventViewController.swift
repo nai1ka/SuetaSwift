61938fa46cebb749a02b0ae01ea44aa4863a31a3
a.goncharova <a.goncharova@innopolis.university>
2023-07-17 11:34:08 +0300

    added modal window📱

--- Sueta/SingleEventView/SingleEventViewController.swift
+++ Sueta/SingleEventView/SingleEventViewController.swift
@@ -15,16 +15,34 @@ class SingleEventViewController: UIViewController {
         }
     }
     
+    
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    
+    
     private lazy var eventNameLabel: UILabel = {
         let eventNameLabel = UILabel()
         eventNameLabel.translatesAutoresizingMaskIntoConstraints = false
         eventNameLabel.textColor = .black
+        eventNameLabel.sizeToFit()
+        eventNameLabel.textAlignment = .center
+        eventNameLabel.font = .boldSystemFont(ofSize: 24)
         return eventNameLabel
+    }()
+    
+    private lazy var descriptionName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Описание"
+        label.font = .systemFont(ofSize: 16)
+        return label
     }()
     
     private lazy var descriptionLabel: UILabel = {
         let descriptionLabel = UILabel()
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.numberOfLines = 0
+        eventNameLabel.sizeToFit()
         return descriptionLabel
     }()
     
@@ -40,42 +58,26 @@ class SingleEventViewController: UIViewController {
         return label
     }()
     
+//    var dateIconView = UIImageView(image: .)?<#default value#>
+    
     private lazy var ownerIDLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-//    private lazy var positionMap: GeoPoint = {
-//
-//    }
+    //    private lazy var positionMap: GeoPoint = {
+    //
+    //    }nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        setupScrollView()
         setupViews()
     }
     
-    private func setupViews(){
-        view.backgroundColor = .systemBackground
-        view.addSubview(eventNameLabel)
-        view.addSubview(descriptionLabel)
-        view.addSubview(numOfParticipantsLabel)
-        view.addSubview(eventDateLabel)
-        view.addSubview(ownerIDLabel)
-        
-        NSLayoutConstraint.activate([
-            eventNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
-            eventNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
-            descriptionLabel.topAnchor.constraint(equalTo: eventNameLabel.bottomAnchor, constant: 24),
-            descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
-            numOfParticipantsLabel.topAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: 24),
-            numOfParticipantsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
-            eventDateLabel.topAnchor.constraint(equalTo: numOfParticipantsLabel.topAnchor, constant: 24),
-            eventDateLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
-            ownerIDLabel.topAnchor.constraint(equalTo: eventDateLabel.topAnchor, constant: 24),
-            ownerIDLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24)
-        ])
-    }
     private func setupData(_ event: Event){
         eventNameLabel.text = event.title
         descriptionLabel.text = event.description
@@ -84,7 +86,44 @@ class SingleEventViewController: UIViewController {
         dateFormatter.dateFormat = "dd-MM"
         eventDateLabel.text = dateFormatter.string(from: event.date)
         ownerIDLabel.text = event.ownerID
+    }
+    func setupScrollView(){
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
         
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
+        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+    }
+    
+    func setupViews(){
+        contentView.addSubview(eventNameLabel)
+        eventNameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        eventNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 48).isActive = true
+        eventNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        
+//        contentView.addSubview(dateIcon)
+        eventNameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        eventNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 48).isActive = true
+        eventNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        
+        contentView.addSubview(descriptionName)
+        descriptionName.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        descriptionName.topAnchor.constraint(equalTo: eventNameLabel.bottomAnchor, constant: 24).isActive = true
+        descriptionName.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        
+        contentView.addSubview(descriptionLabel)
+        descriptionLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: descriptionName.bottomAnchor, constant: 16).isActive = true
+        descriptionLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
 }
