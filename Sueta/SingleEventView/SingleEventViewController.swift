8b7275f427175ca3fa46cf19534d6b1164d3fb76
a.goncharova <a.goncharova@innopolis.university>
2023-07-18 16:57:46 +0300

    added more labels + map🌿

--- Sueta/SingleEventView/SingleEventViewController.swift
+++ Sueta/SingleEventView/SingleEventViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class SingleEventViewController: UIViewController {
     var event: Event? {
@@ -54,9 +55,27 @@ class SingleEventViewController: UIViewController {
         return label
     }()
     
+    private lazy var locationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Местоположение"
+        label.font = .boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
     private lazy var numOfParticipantsLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        if let numOfParticipants = event?.peopleNumber, let numOfJoined = event?.users.count {
+            if (numOfParticipants - numOfJoined > 0) {
+                label.text = "Осталось \(numOfParticipants - numOfJoined) мест"
+            } else {
+                label.text = "Места закончились"
+            }
+        } else {
+            label.text = "Нет информации о местах"
+        }
+        print(label.text)
         return label
     }()
     
@@ -80,6 +99,12 @@ class SingleEventViewController: UIViewController {
         return label
     }()
     
+    private lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
     //    private lazy var positionMap: GeoPoint = {
     //
     //    }nil
@@ -101,6 +126,10 @@ class SingleEventViewController: UIViewController {
         dateFormatter.dateFormat = "dd-MM"
         eventDateLabel.text = dateFormatter.string(from: event.date)
         ownerIDLabel.text = event.ownerID
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: event.position.latitude, longitude: event.position.longitude)
+        mapView.addAnnotation(annotation)
     }
     func setupScrollView(){
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -117,6 +146,7 @@ class SingleEventViewController: UIViewController {
         contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        contentView.bottomAnchor.constraint(greaterThanOrEqualTo: scrollView.bottomAnchor, constant: -16).isActive = true
     }
     
     func setupViews(){
@@ -125,24 +155,33 @@ class SingleEventViewController: UIViewController {
         eventNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 48).isActive = true
         eventNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
         
+        //date icon
         contentView.addSubview(dateIcon)
-        
         dateIcon.topAnchor.constraint(equalTo: eventNameLabel.topAnchor, constant: 56).isActive = true
         dateIcon.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 48).isActive = true
         dateIcon.widthAnchor.constraint(equalToConstant: 40).isActive = true
         dateIcon.heightAnchor.constraint(equalToConstant: 40).isActive = true
         
+        //event date label
         contentView.addSubview(eventDateLabel)
 //        eventDateLabel.topAnchor.constraint(equalTo: dateIcon.topAnchor).isActive = true
         eventDateLabel.centerYAnchor.constraint(equalTo: dateIcon.centerYAnchor).isActive = true
         eventDateLabel.leftAnchor.constraint(equalTo: dateIcon.leftAnchor, constant: 48).isActive = true
         eventDateLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
         
+        //available places label
+        
+        contentView.addSubview(numOfParticipantsLabel)
+//        numOfParticipantsLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        numOfParticipantsLabel.topAnchor.constraint(equalTo: dateIcon.bottomAnchor, constant: 24).isActive = true
+        numOfParticipantsLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 48).isActive = true
+        numOfParticipantsLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        
         //"Description"
         contentView.addSubview(descriptionName)
 //        descriptionName.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        descriptionName.topAnchor.constraint(equalTo: eventDateLabel.bottomAnchor, constant: 24).isActive = true
-        descriptionName.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        descriptionName.topAnchor.constraint(equalTo: numOfParticipantsLabel.bottomAnchor, constant: 24).isActive = true
+//        descriptionName.widthAnchor.constraint(equalTo: numOfParticipantsLabel.widthAnchor, multiplier: 3/4).isActive = true
         descriptionName.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 48).isActive = true
 
         contentView.addSubview(descriptionLabel)
@@ -156,8 +195,25 @@ class SingleEventViewController: UIViewController {
         organisatorLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 24).isActive = true
         organisatorLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
         organisatorLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 48).isActive = true
-
         
+        // Organisator label
+        contentView.addSubview(ownerIDLabel)
+//        descriptionLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        ownerIDLabel.topAnchor.constraint(equalTo: organisatorLabel.bottomAnchor, constant: 16).isActive = true
+        ownerIDLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        ownerIDLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24).isActive = true
         
+        //"Location"
+        contentView.addSubview(locationLabel)
+        locationLabel.topAnchor.constraint(equalTo: ownerIDLabel.bottomAnchor, constant: 24).isActive = true
+        locationLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        locationLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 48).isActive = true
+        
+        //map view
+        contentView.addSubview(mapView)
+        mapView.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 24).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        mapView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
     }
 }
